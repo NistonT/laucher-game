8dc9c6faf9f27a41574939ccac17588fa02edaf8
NistonN <nisprogrammer@gmail.com>
2025-05-01 00:17:06 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+	// async function greet() {
+	// 	setGreetMsg(await invoke("greet", { name }));
+	// }
+
+	// async function close() {
+	// 	return await getCurrentWindow().minimize();
+	// }
+
+	return <h1>Hello world!</h1>;
+}
+
+export default App;
